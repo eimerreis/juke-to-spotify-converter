433c041fc1fca9dfce6a04e8bc5bf2de24e92f54
eimerreis <imorice95@googlemail.com>
2016-03-05 16:08:38 +0100

    juke parser returns full song title now

--- modules/juke/juke-errors.js
+++ modules/juke/juke-errors.js
@@ -2,19 +2,19 @@
 
 var util = require('util');
 
-var BaseError = require('../error/module').BaseError;
+var BaseError = require('../core/base-error');
 
 /**
- * Base Error for Billing Module
+ * Base Error for Juke Module
  */
-function BillingError(name, message, details, previous){
+function JukeError(name, message, details, previous){
     name = name || 'BillingError';
     message = message || 'Unspecified ' + name + 'occured';
     BaseError.call(this, name, message, details, previous);
 }
-util.inherits(BillingError, BaseError);
+util.inherits(JukeError, BaseError);
 
 
 module.exports = {
-    BillingError: BillingError
+    JukeError: JukeError
 };
